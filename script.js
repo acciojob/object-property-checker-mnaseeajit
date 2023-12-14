e8d61f66ed72087880d773fa5623d562e8be11c6
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-14 12:28:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let  entries = Object.entries(sampleObject);
+	for(let i = 0;i<entries.length;i++)
+		{
+			if(entries[i][0] == key){
+				return true;
+			}   
+		}
+	return false;
 }
 
 // Do not change the code below
 const key = prompt("Enter Key.");
-alert(hasKey(key));
+alert(hasKey(key));  
